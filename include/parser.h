d48d58825e03e1b2ea6441dd9f7a09fe3231bb7b
Ivy Banana <cnnorwood14@gmail.com>
2025-11-27 23:58:40 -0500

    `parser.h` implemented. Next: `parser.c`

--- include/parser.h
+++ include/parser.h
@@ -1,5 +1,19 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <lexer.h>
+#include <ast.h>
+
+typedef struct {
+    Lexer* lexer;
+    Token current;
+    Token previous;
+} Parser;
+
+// Forward Declarations
+void parser_init(Parser* parser, Lexer* lexer);
+Node** parse_program(Parser* parser);
+Node** parse_statement(Parser* parser);
+Node** parse_expression(Parser* parser);
 
 #endif
